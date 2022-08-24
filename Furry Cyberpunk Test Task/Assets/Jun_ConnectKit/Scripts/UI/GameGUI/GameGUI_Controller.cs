24cd51106768b035c3ea619cb0bb9028141d8a6f
NikiBober <95879723+NikiBober@users.noreply.github.com>
2022-08-24 20:11:23 +0300

    0.3 ConnectKit successfully imported
    with update obsolete API and no ProjectSettings;
    compiler error fixed, built-in scenes works.

---
+++ Furry Cyberpunk Test Task/Assets/Jun_ConnectKit/Scripts/UI/GameGUI/GameGUI_Controller.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameGUI_Controller : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () 
+	{
+
+		GameController.instance.onGameStatusChange += OnGameStatusChange;
+
+		GameGUI_GamingUI.instance.Hide ();
+		GameGUI_LevelComplete.instance.Hide ();
+		GameGUI_PuaseUI.instance.Hide ();
+	}
+
+	void OnGameStatusChange (GameController.GameStatus changeStatus)
+	{
+		switch (changeStatus)
+		{
+		case GameController.GameStatus.Gaming:
+			GameGUI_GamingUI.instance.Show ();
+			break;
+
+		case GameController.GameStatus.GameSettlement:
+			break;
+
+		case GameController.GameStatus.GameOver:
+			GameGUI_GamingUI.instance.Hide ();
+			break;
+
+		case GameController.GameStatus.LevelCompletions:
+			GameGUI_LevelComplete.instance.Show ();
+			break;
+		}
+	}
+}
