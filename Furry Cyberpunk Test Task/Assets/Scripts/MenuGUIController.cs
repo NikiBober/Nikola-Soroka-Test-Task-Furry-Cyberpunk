ffcd065c7cd7b75d398a4107b1ffff80f3282a3d
NikiBober <95879723+NikiBober@users.noreply.github.com>
2022-09-05 21:16:11 +0300

    0.8.1 Pause menu by key down
    -pause menu now toggle by pressing key (set in MenuGUIController)

--- Furry Cyberpunk Test Task/Assets/Scripts/MenuGUIController.cs
+++ Furry Cyberpunk Test Task/Assets/Scripts/MenuGUIController.cs
@@ -2,10 +2,51 @@
 
 public class MenuGUIController : MonoBehaviour {
 
+	[SerializeField]
+	KeyCode pauseKey;
+
+    bool isPaused;
+
 	// Use this for initialization
 	void Start () 
 	{
-		//GameGUI_LevelComplete.instance.Hide ();
 		MenuGUI_PuaseUI.instance.Hide ();
 	}
+
+	void Update()
+	{
+		if (Input.GetKeyDown(pauseKey))
+			Toggle_Pause();
+	}
+
+    public void Toggle_Pause()
+    {
+        if (isPaused)
+        {
+            ResumeGame();
+        }
+        else
+        {
+            PauseGame();
+        }
+
+        isPaused = !isPaused;
+    }
+
+
+    // Pause
+    public void PauseGame()
+    {
+        MenuGUI_PuaseUI.instance.Show();
+        MenuGUI.instance.Hide();
+    }
+
+    // Resume/Unpause
+    public void ResumeGame()
+    {
+        MenuGUI_PuaseUI.instance.Hide();
+        MenuGUI.instance.Show();
+    }
+
+
 }
